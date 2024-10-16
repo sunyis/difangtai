9bab9693ff4df4360756853f7ad157d9e8af71df
wzsvip <161473552+wzsvip@users.noreply.github.com>
2024-10-16 13:14:39 +0800

    Update generate_mg.py

--- generate_mg.py
+++ generate_mg.py
@@ -144,7 +144,7 @@ def update_single_m3u8_file(url, filename):
 
 def get_live_url(channel_id):
     proxy= {
-        'http': '119.28.221.28:8088',
+        'http': '221.231.13.198:1080',
     }
     url = f'http://mpp.liveapi.mgtv.com/v1/epg/turnplay/getLivePlayUrlMPP?version=PCweb_1.0&platform=1&buss_id=2000001&channel_id={channel_id}'
     # print(channel_id, url)
